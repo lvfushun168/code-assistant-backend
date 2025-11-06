3fe86ff704df907c0cd6f12781dd5e8009689a81
lv fushun <lvfushun168@outlook.com>
2025-11-06 10:56:37 +0800

    init

--- src/main/java/com/lfs/codeassistantbackend/CodeAssistantBackendApplication.java
+++ src/main/java/com/lfs/codeassistantbackend/CodeAssistantBackendApplication.java
@@ -1,9 +1,11 @@
 package com.lfs.codeassistantbackend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.lfs.codeassistantbackend.repository")
 public class CodeAssistantBackendApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/lfs/codeassistantbackend/domain/entity/BaseEntity.java
+++ src/main/java/com/lfs/codeassistantbackend/domain/entity/BaseEntity.java
@@ -1,5 +1,8 @@
 package com.lfs.codeassistantbackend.domain.entity;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableLogic;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -7,9 +10,13 @@ import java.time.LocalDateTime;
 @Data
 public class BaseEntity {
 
+    @TableField(fill = FieldFill.INSERT)
     private LocalDateTime createTime;
 
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     private LocalDateTime updateTime;
 
-    private Boolean delete;
+    @TableLogic
+    @TableField(fill = FieldFill.INSERT)
+    private Boolean deleted;
 }
